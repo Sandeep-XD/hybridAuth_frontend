e42c3971a8169a158c7ae32a81ba2f7e84e6e34a
Sandeep Barman <sanbofficial@gmail.com>
2026-03-11 21:36:18 +0530

    Update baseUrl in ApiConstants

--- lib/features/core/constants/api_constants.dart
+++ lib/features/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // static const baseUrl = "http://ad470f3aa1cea4a0195ee309b31fe5bb-1883728287.ap-south-1.elb.amazonaws.com/api/v1"; //do not change
-  // static const baseUrl = "https://chuck-hormic-caliginously.ngrok-free.dev/api/v1"; //do not change
+  static const baseUrl = "https://chuck-hormic-caliginously.ngrok-free.dev/api/v1"; //do not change
     static const baseUrl = "/api";
   static const authInit = "/auth/init";
 
